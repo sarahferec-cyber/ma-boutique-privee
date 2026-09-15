7ae2d2e607a60087d2db5e525b3df90749d7bb78
sarahferec-cyber <sarah.ferec@laposte.net>
2026-09-16 01:21:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ if st.session_state.panier:
     if st.sidebar.button("✨ Valider mon achat"):
         stock_mis_a_jour_avec_succes = True 
         
-                if stock_mis_a_jour_avec_succes:
+        if stock_mis_a_jour_avec_succes:
             st.balloons()
             st.success("Achat validé avec succès ! 🎉")
             
