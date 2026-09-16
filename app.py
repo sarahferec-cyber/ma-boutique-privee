af23ea996823753a44c42b793d2cd5df8d6028e9
sarahferec-cyber <sarah.ferec@laposte.net>
2026-09-16 03:12:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -56,9 +56,9 @@ if st.sidebar.button("🚪 Se déconnecter"):
     st.session_state.utilisateur = ""
     st.rerun()
 
-URL_SHEETS = "https://google.com"
+URL_SHEETS = "https://docs.google.com/spreadsheets/d/1ZtcJ0Wz9mZcqbyd_jnT33_Q7ebfRhgPLddRUWi7NjYA/edit?usp=sharing"
 URL_DISCORD = "https://discord.com"
-URL_MACRO_STOCK = "https://google.com"
+URL_MACRO_STOCK = "https://script.google.com/macros/s/AKfycbxTep4v3fevHxUE0Cv6f6SE1IRie_xCNctecO_7Ez_XXhNUQJlhc46l6mkDe-FQk7s5lA/exec"
 
 def load_clean_data():
     try:
